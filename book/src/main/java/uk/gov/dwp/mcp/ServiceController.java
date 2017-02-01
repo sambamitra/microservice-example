38afc5d6c2dcd8e04c7dbac69b487600ae33939d
Samba Mitra <samba.mitra@accenture.com>
2017-02-01 11:35:29 +0000

    Created keystores and made services connect to discovery via ssl

--- book/src/main/java/uk/gov/dwp/mcp/ServiceController.java
+++ book/src/main/java/uk/gov/dwp/mcp/ServiceController.java
@@ -1,18 +1,24 @@
 package uk.gov.dwp.mcp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ServiceController {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(ServiceController.class);
+
 	@GetMapping("/available")
 	public String available() {
+		LOGGER.info("Fetching available books");
 		return "Spring in Action";
 	}
 
 	@GetMapping("/checked-out")
 	public String checkedOut() {
+		LOGGER.info("Fetching checked out books");
 		return "Spring Boot in Action";
 	}
 
